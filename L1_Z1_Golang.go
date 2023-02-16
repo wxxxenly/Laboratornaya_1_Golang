b61cd8c83bd4cd745f2034e7a8f8df5cddfdd7dc
wxxxenly <65085220+wxxxenly@users.noreply.github.com>
2023-02-16 17:17:05 +0800

    Update L1_Z1_Golang.go

--- L1_Z1_Golang.go
+++ L1_Z1_Golang.go
@@ -9,15 +9,15 @@ func main() {
     // вводим число X
     var x float64
     fmt.Println("Введите число X: ")
-	fmt.Scanf("%x\n", &x)
-	// вводим число Y
-	var y float64
+    fmt.Scanf("%x\n", &x)
+    // вводим число Y
+    var y float64
     fmt.Println("Введите число Y: ")
-	fmt.Scanf("%y\n", &y)
-	// вводим число Z
-	var z float64
+    fmt.Scanf("%y\n", &y)
+    // вводим число Z
+    var z float64
     fmt.Println("Введите число Z: ")
-	fmt.Scanf("%z\n", &z)
+    fmt.Scanf("%z\n", &z)
     // создаем переменные a,b,c, присваиваем им тип float64 и производим операции над ними
     var a float64 = (2.4 * math.Pow(10,-3) * math.Pow(x,(4/x))*(x-PI/math.Sqrt(3)))/(0.21*math.Pow(10,-5)*math.Sqrt(math.Abs(y)/math.Abs(z))+math.Pow(x,2)*math.Abs(y-math.Pow(math.Tan(math.Pow(x,2)),3)))
     var b float64 = ((y-x)/(z-x) + (y-(y/z)-x)/(math.Pow(math.Sin(x/y),2)))
